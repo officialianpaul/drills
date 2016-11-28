77459993aac92958bf1dce77b5f51b16eb9fc70f
officialianpaul <officialianpaul@gmail.com>
2016-11-29 01:27:55 +0800

    the background was edited, added home button

--- project_City_Without_Crime/CWCAbout.php
+++ project_City_Without_Crime/CWCAbout.php
@@ -1,5 +1,6 @@
-<body background="wallpaper.jpg">
+<body background="Police-Line-Free-HD-Widescreen-Wallpapers-6946.jpg">
 <font color="red">
 <marquee><h1>This Website connects all the users and police officers and administrative persons </h1></marquee>
 </font>
+ <FORM><INPUT Type="button" VALUE="Back to Homepage" onClick="history.go(-1);return true;"></FORM>
 </body>
